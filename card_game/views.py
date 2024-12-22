cd82548a35cffbf757c8f666576e239ae07e5238
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-12-22 00:42:28 -0800

    implemented main code for websockets

--- card_game/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Card
-
-def game_view(request):
-    cards = Card.objects.all()
-    return render(request, 'card_game/game.html', {'cards': cards})
